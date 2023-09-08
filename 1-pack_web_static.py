6f2218912cb18b8808d2eed296731ca1ec5c254c
stephenkiai <stephenkiai23@gmail.com>
2023-09-08 03:25:05 +0300

    web static pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-""" generates a .tgz archive from contents of web_static folder"""
+""" web_static folder archiver"""
+
 from fabric.api import local
-import time
+import datetime import datetime
 
 
 def do_pack():
-    """ generate a tgz archive from web_static folder contents"""
+    """ generate a tgz archive"""
     try:
+        # Create the "versions" directory if it doesn't exist
         local("mkdir -p versions")
+
+        # Compress the contents of the "web_static" folder
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
+              format(datetime.strftime("%Y%m%d%H%M%S")))
+
+        # Return the archive path if successful
+        return ("versions/web_static_{}.tgz".format(datetime.
                                                     strftime("%Y%m%d%H%M%S")))
     except:
         return None
